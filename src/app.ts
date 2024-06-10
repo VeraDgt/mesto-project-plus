5c4ab3d2dfa91e781ff7e9716936de2997760d38
VeraDgt <veradgt@gmail.com>
2024-06-10 14:52:17 +0300

    feat/mestodb connected

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import { PORT, DATABASE } from '../src/utils/constants';
+import mongoose from 'mongoose';
+import router from '../src/routes/index';
 
 const app = express();
 
+app.use(router);
+
 const connect = async () => {
   try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(DATABASE);
+    console.log('Есть подключение к базе данных');
     await app.listen(PORT);
     console.log(`Сервер запущен па порту: ${PORT}`);
   } catch (err) {
